8ea485c80525c60408fd753be628472d96f00a12
mjchi7 <chinmingjun8@gmail.com>
2021-07-17 12:43:31 +0800

    Added mongo connection code

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/stretchr/testify v1.7.0 // indirect
+	go.mongodb.org/mongo-driver v1.6.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"mjchi7/mogen/config"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func readFile(path string) string {
@@ -12,6 +19,32 @@ func readFile(path string) string {
 		panic(err)
 	}
 	return string(data)
+}
+
+func buildConn(ctx context.Context, host string, port string, tlsEnabled bool, databaseName string, maxPoolSize uint64) *mongo.Client {
+	// instantiate a timeout context
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+	tlsConfig := tls.Config{
+		InsecureSkipVerify: true,
+	}
+	uri := "mongodb://" + host + ":" + port + "/databaseName"
+	if tlsEnabled {
+		uri = uri + "/?tls=true"
+	}
+	mongoConfig := options.Client().ApplyURI(uri).SetMaxPoolSize(maxPoolSize).SetTLSConfig(&tlsConfig).SetDirect(true)
+	conn, err := mongo.Connect(ctx, mongoConfig)
+	if err != nil {
+		panic(err)
+	}
+	return conn
+}
+
+func verifyConn(ctx context.Context, c *mongo.Client) error {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	err := c.Ping(ctx, readpref.Primary())
+	return err
 }
 
 func main() {
